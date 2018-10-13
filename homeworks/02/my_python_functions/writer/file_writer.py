52bca5a06702d5f5d0304cfb1e370fdac578f26d
shybotan <ira-prokk@mail.ru>
2018-10-13 23:14:49 +0300

    Week2

--- homeworks/02/my_python_functions/writer/file_writer.py
+++ homeworks/02/my_python_functions/writer/file_writer.py
@@ -1,56 +1,88 @@
 import os
 import pickle as pkl
+
+
+
 class FileWriter:
     
     def __init__(self, path):
-        """path - путь до файла"""
-        if self._check_path(path):
-            self.path_ = path
-            self.file = None
-        else:
-            os.system ("touch " + path)
-            self.path=path
-            self.file=None
-        
+        if not self._check_path(path):
+            os.system("touch " + path)
+        self._path = path
+        self.file = open(path)
+        self.file.close()
+    
     def _check_path(self, path):
         try:
-            f=open(path)
+            f = open(path)
             f.close()
             return 1
         except:
             return 0
-
-    def get_path(self):
-            return self.path
-
-    def set_path(self,path0):
-            self.path=path0
-
-    def delete(self):
-        os.system("rm -rf "+self.path)
     
-    def print_file(self):
-            f=open(self.path)
-            for line in f:
-                print (line)
-            f.close()
+    @property
+    def path(self):
+        return self._path
+    
+    @path.setter
+    def path(self, npath):
 
+        if self.file != None:
+            self.file.close()
+            self.file = None
+
+        if not self._check_path(npath):
+            os.system("touch " + npath)
+        self._path = npath
+        self.file = open(npath)
+        self.file.close()
+
+    @path.deleter 
+    def path(self):
+
+        if self._path == None:
+            return
+        
+        os.system("rm -rf " + self._path)
+        self.file = None
+        self._path = None
+
+    def print_file(self):
+
+        if self._path == None and self.file == None:
+            raise Exception("print-error")
+        if self.file == None:
+            if not self._check_path(self._path):
+                return
+        
+        self.file = open(self._path)
+        for ln in self.file:
+            print(ln)
+        self.file.close()
+    
     def write(self, some_string):
-        with open(self.path,'a') as f_obj:
-            f_obj.write(some_string)
-            f_obj.write('\n')
+        if self._path == None:
+            raise Exception("write-error")
+        if not self._check_path(self._path):
+            os.system("touch " + self._path)
+
+        with open(self._path, 'a') as f:
+            f.write(some_string)
     
     def save_yourself(self, file_name):
         with open(file_name, 'wb') as f:
+            if self.file != None:
+                self.file = None
             pkl.dump(self, f)
     
     @classmethod
     def load_file_writer(cls, pickle_file):
         with open(pickle_file, 'rb') as f:
-            return  pkl.load(f)
-stroka='blabla.txt'
-try:
-    f=FileWriter(stroka)
-except:
-    os.system("touch " + stroka)
-    f=FileWriter(stroka)
+            rs = pkl.load(f)
+            if rs._path == None:
+                raise Exception("load-error")
+            if not rs._check_path(rs._path):
+                return
+            rs.file = open(rs._path)
+            rs.file.close()
+            return rs
