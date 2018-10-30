d14be081dec59a9a52880177a81c393a1a98708d
shybotan <ira-prokk@mail.ru>
2018-10-30 17:21:28 +0300

    WEEK3 FLASK

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -1,9 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
+    if request.method == 'POST':
+        buf=request.json
+        if version=='1':
+            slovaric = {"version":1,"predict":buf['predict']}
+            return jsonify(slovaric)
+        if version=='0':
+            slovaric = {"version":0,"predict":buf['old_predict']}
+            return jsonify(slovaric)
+            
     #TODO прочитать из полученного запроса json-контент
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
@@ -11,7 +20,8 @@ def return_classifier_result(version):
 @app.route("/")
 def hello():
     #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return render_template('SOMETHING.html')
 
 if __name__ == "__main__":
     app.run()
+#долго был затуп с тем, что обращаясь к серваку, он не был запущен))))))))))))00))0
